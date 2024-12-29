eac80665614c90c58560ac251b04827d51a52c70
amar <amarkodge@gmail.com>
2024-12-30 03:18:18 +0530

    implementing StorageFactory to create contract from another contract

---
+++ StorageFactory.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory {
+
+    SimpleStorage[] public listOfSimpleStorage;
+
+    function createSimpleStorageContract() public   {
+        SimpleStorage newSimpleStorage = new SimpleStorage();
+        listOfSimpleStorage.push(newSimpleStorage);
+    }
+
+    function sfStorage(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        listOfSimpleStorage[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return listOfSimpleStorage[_simpleStorageIndex].retrive();
+    }
+
+}
